489546a43fa0ea984e6150cbcfdc2ad34d6e0f2a
ohe1013 <84114149+ohe1013@users.noreply.github.com>
2023-01-06 17:44:33 +0900

    todo cmmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React, { useContext } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import RequireAuth from "./components/auth/RequiredAuth";
-import AuthContext from "./context/AuthProvider";
+import RequireAuth, { NotRequireAuth } from "./components/auth/RequiredAuth";
 import TodoMain from "./components/todo/TodoMain";
+import TodoDetail from "./components/todo/TodoDetail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route element={<RequireAuth />}>
+        <Route element={<NotRequireAuth />}>
           <Route path="auth" element={<Login />} />
           <Route path="register" element={<Register />} />
+        </Route>
+        <Route element={<RequireAuth />}>
           <Route path="todos" element={<TodoMain />} />
+          <Route path="todos/:id" element={<TodoDetail />} />
         </Route>
       </Route>
+      <Route path="*" element={<Layout />} />
     </Routes>
   );
 }
